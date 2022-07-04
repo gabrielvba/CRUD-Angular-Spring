a60b6fbe23ce4e0c66d21aecde76b0ccda11a230
gabrielvba <gabriel.valentino@hotmail.com>
2022-07-04 10:40:23 -0300

    add chamada http get

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,5 +1,7 @@
+import { CoursesService } from './../services/courses.service';
 import { Course } from './../model/course';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Component({
   selector: 'app-courses',
@@ -8,12 +10,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Course[] = [
-    { _id: '1', name: 'Angular', category: 'front-end'}
-  ];
+  courses: Observable<Course[]>;
   displayedColumns = ['name','category'];
 
-  constructor() { }
+  constructor(private coursesService: CoursesService) {
+    this.courses = this.coursesService.list();
+   }
 
   ngOnInit(): void {
   }
